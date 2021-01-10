9d6490c0bdbbc2458a3ab067b25e117b9a7bf492
Kaiyu Wang <cathywky123@gmail.com>
2021-01-09 23:42:32 -0500

    add for loop and conditions

--- pages/views.py
+++ pages/views.py
@@ -14,10 +14,16 @@ def home_view(request, *args, **kwargs):
 def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
 
-
 def about_view(request, *args, **kwargs):
-	return render(request, "about.html", {})
+    my_context = {
+        "title": "abc this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
 
-
+    }
+    return render(request, "about.html", my_context)
+  
 def social_view(request, *args, **kwargs):
 	return render(request, "social.html", {})
