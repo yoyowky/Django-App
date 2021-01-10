bcaef21880cf8fbd9b707675f49ec6352a331542
Kaiyu Wang <cathywky123@gmail.com>
2021-01-09 19:44:07 -0500

    change model.py

--- products/models.py
+++ products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-	title 		= models.TextField()
-	description = models.TextField()
-	price 		= models.TextField()
+	title 		= models.CharField(max_length=120)   # max_length=required
+	description	= models.TextField(blank=True, null=True)
+	price 		= models.DecimalField(decimal_places=2, max_digits=1000)
 	summary 	= models.TextField(default='This is cool')
+	featured	= models.BooleanField() # new added， but exit obj in db doesn't has this value, two option: null=True or default=True
