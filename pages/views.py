c53a1233957297b60bf0d2d2c864c16dbd7e723b
Kaiyu Wang <cathywky123@gmail.com>
2021-01-09 20:36:57 -0500

    add more view and add request as arg

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 # def home_view():
 # 	return "<h1>Hello World</h1>"   # string of html code, not django
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+	print(args, kwargs, request);   # () {} <WSGIRequest: GET '/'>
+	print(request.user);  # yoyo
 	return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_view(request, *args, **kwargs):
+	return HttpResponse("<h1>Contact page</h1>")
+
+
+def about_view(request, *args, **kwargs):
+	return HttpResponse("<h1>About page</h1>")
+
+
+def social_view(request, *args, **kwargs):
+	return HttpResponse("<h1>Social page</h1>")
