573ae4c03b5f7417b6770edb09dd12fe3cb48b9b
Kaiyu Wang <cathywky123@gmail.com>
2021-01-10 16:53:36 -0500

    render data from model

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product 
 
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description,
+    #     'price': obj.price,
+    #     'summary': obj.summary,
+    #     'featured': obj.featured
+    # }
+    context = {
+        'object': obj
+    }
+    return render(request, "product/detail.html", context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view, social_view
+from products.views import product_detail_view
 
 urlpatterns = [
 	path('', home_view, name='home'),
@@ -23,4 +24,5 @@ urlpatterns = [
 	path('about/', about_view, name='about'),
 	path('social/', social_view, name='social'),
     path('admin/', admin.site.urls),
+    path('product', product_detail_view, name="product_detail")
 ]
