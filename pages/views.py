1f4ebe670b4605cccde1d9684edf3840cfe6413d
Kaiyu Wang <cathywky123@gmail.com>
2021-01-09 21:03:58 -0500

    add html template

--- pages/views.py
+++ pages/views.py
@@ -7,16 +7,17 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
 	print(args, kwargs, request);   # () {} <WSGIRequest: GET '/'>
 	print(request.user);  # yoyo
-	return HttpResponse("<h1>Hello World</h1>")
+	# return HttpResponse("<h1>Hello World</h1>")
+	return render(request, "home.html", {})     # use html template
 
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact page</h1>")
+	return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About page</h1>")
+	return render(request, "about.html", {})
 
 
 def social_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Social page</h1>")
+	return render(request, "social.html", {})
