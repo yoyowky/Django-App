38d709c65ece455801637fa1272c45986ea39d88
Kaiyu Wang <cathywky123@gmail.com>
2021-01-10 20:11:10 -0500

    pure django form

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import Product
 
+# django model form
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
@@ -10,3 +11,12 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+# django form
+class RawProductForm(forms.Form):
+    title       = forms.CharField(max_length=120, required=True)
+    description = forms.CharField(max_length=120, required=True)
+    price       = forms.DecimalField(required=False)
+    summary     = forms.CharField(max_length=120, required=False)
+    featured    = forms.BooleanField(required=False)
--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 from .models import Product 
 
 # Create your views here.
 
-# use django form
+# use django model form to save data
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
 #     if form.is_valid():
@@ -16,17 +16,32 @@ from .models import Product
 #     }
 #     return render(request, "product/product_create.html", context)
 
+# use raw html form to save data
+# def product_create_view(request):
+#     # print('get11111', request.GET)
+#     # print('post11111', request.POST)
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print('my_new_title', my_new_title)
+#         # Product.objects.create(title=my_new_title)
+#     context = {}
+#     return render(request, "product/product_create.html", context)
 
+# use django form to save data 
 def product_create_view(request):
-    # print('get11111', request.GET)
-    # print('post11111', request.POST)
+    my_form = RawProductForm()
     if request.method == "POST":
-        my_new_title = request.POST.get('title')
-        print('my_new_title', my_new_title)
-        # Product.objects.create(title=my_new_title)
-    context = {}
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print('cleaned_data11111', my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print('form_errors1111', my_form.errors)
+    context = {
+        "form": my_form
+    }
     return render(request, "product/product_create.html", context)
-
 
 
 
