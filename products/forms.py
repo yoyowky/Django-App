9c53e080596f66a063ee94ea4c478267685fe4ca
Kaiyu Wang <cathywky123@gmail.com>
2021-01-10 20:23:06 -0500

    form widget

--- products/forms.py
+++ products/forms.py
@@ -15,8 +15,23 @@ class ProductForm(forms.ModelForm):
 
 # django form
 class RawProductForm(forms.Form):
-    title       = forms.CharField(max_length=120, required=True)
-    description = forms.CharField(max_length=120, required=True)
+    title       = forms.CharField(
+                            max_length=120,
+                            required=True,
+                            widget=forms.TextInput({
+                                "placeholder": "Your Title"
+                            }))
+    description = forms.CharField(
+                            max_length=120,
+                            required=True,
+                            widget=forms.Textarea(attrs={
+                                "placeholder": "Your Description",
+                                "class": "new-class-name two",
+                                "id": "my-id-for-textarea",
+                                "rows": 20,
+                                "col": 120
+
+                            }))
     price       = forms.DecimalField(required=False)
     summary     = forms.CharField(max_length=120, required=False)
     featured    = forms.BooleanField(required=False)
